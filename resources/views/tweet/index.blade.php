0140cc873179304b4b503c76b3b58d5d3a731b23
ryosuke <sessyomaru427@gmail.com>
2023-02-26 23:09:20 +0900

    編集機能作成

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -1,30 +1,38 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>つぶやきアプリ</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>つぶやきアプリ</title>
 </head>
+
 <body>
-  <h1>つぶやきアプリ</h1>
-  <div>
-    <p>投稿フォーム</p>
-    <form action="{{ route('tweet.create') }}" method="post">
-      @csrf
-      <label for="tweet-content">つぶやき</label>
-      <span>140文字まで</span>
-      <textarea name="tweet" id="tweet-content" cols="30" rows="10" placeholder="つぶやきを入力"></textarea>
-      @error('tweet')
-      <p style="color: red">{{ $message }}</p>
-      @enderror
-      <button type="submit">投稿</button>
-    </form>
-  </div>
-  <div>
-    @foreach ($tweets as $tweet)
-      <p>{{ $tweet->content }}</p>        
-    @endforeach
-  </div>
+    <h1>つぶやきアプリ</h1>
+    <div>
+        <p>投稿フォーム</p>
+        <form action="{{ route('tweet.create') }}" method="post">
+            @csrf
+            <label for="tweet-content">つぶやき</label>
+            <span>140文字まで</span>
+            <textarea name="tweet" id="tweet-content" cols="30" rows="10" placeholder="つぶやきを入力"></textarea>
+            @error('tweet')
+                <p style="color: red">{{ $message }}</p>
+            @enderror
+            <button type="submit">投稿</button>
+        </form>
+    </div>
+    <div>
+        @foreach ($tweets as $tweet)
+            <details>
+                <summary>{{ $tweet->content }}</summary>
+                <div>
+                    <a href="{{ route('tweet.update.index', ['tweetId' => $tweet->id]) }}">編集</a>
+                </div>
+            </details>
+        @endforeach
+    </div>
 </body>
+
 </html>
